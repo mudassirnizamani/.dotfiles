db615962bbc7781ed4b3c4a6ba15eb3ba3ee610f
Mudassir Nizamani <mudasiralinizamani@gmail.com>
2024-10-27 07:34:44 +0500

    made some adjustments

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -13,7 +13,7 @@ return {
     opts = {},
     config = function()
       require("flow").setup {
-        transparent = true,       -- Set transparent background.
+        transparent = false,       -- Set transparent background.
         fluo_color = "orange",    --  Fluo color: pink, yellow, orange, or green.
         mode = "normal",          -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
         aggressive_spell = false, -- Display colors for spell check.
