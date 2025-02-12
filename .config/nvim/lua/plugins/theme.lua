f18c1774fcdc3bbfab9330883f1a31875d9253ea
Mudassir Nizamani <mudasiralinizamani@gmail.com>
2025-02-12 23:13:45 +0500

    changed ghostty opacity and nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -10,7 +10,7 @@ return {
         overrides = {},  -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
       })
 
-      vim.cmd("colorscheme ayu")
+      -- vim.cmd("colorscheme ayu")
     end
   },
   {
@@ -23,16 +23,30 @@ return {
   {
     "0xstepit/flow.nvim",
     lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("flow").setup {
-        transparent = false,      -- Set transparent background.
-        fluo_color = "orange",    --  Fluo color: pink, yellow, orange, or green.
-        mode = "normal",          -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
-        aggressive_spell = false, -- Display colors for spell check.
-      }
-      -- vim.cmd "colorscheme flow"
+    priority = 5000,
+    opts = {
+      transparent = true,    -- Set transparent background.
+      fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
+      mode = "bright",       -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+
+      theme = {
+        style = "dark",    --  "dark" | "light"
+        contrast = "high", -- "default" | "high"
+      },
+      colors = {
+        custom = {
+          saturation = "0", -- "" | string representing an integer between 0 and 100
+          light = "0",      -- "" | string representing an integer between 0 and 100
+        },
+      },
+      ui = {
+        borders = "inverse",      -- "theme" | "inverse" | "fluo" | "none"
+        aggressive_spell = false, -- true | false
+      },
+    },
+    config = function(_, opts)
+      require("flow").setup(opts)
+      vim.cmd "colorscheme flow"
     end,
   },
   {
